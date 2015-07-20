90a63ca15f3a7a2aa6c4fe0b4a129efa5ba84dd3
Adoni <adoni1203@gmail.com>
2015-07-20 11:41:13 +0800

    fix gitignore

---
+++ mallet-2.0.7/train_label.sh
@@ -0,0 +1,4 @@
+data_dir=/mnt/data1/adoni/jd_data/matrixes/mallet/data.mallet
+bin/mallet import-file --input $data_dir --output data.mallet --token-regex '[0-9]'
+bin/mallet train-classifier --input data.mallet --trainer "MaxEntGETrainer,gaussianPriorVariance=0.1,constraintsFile=\"review.constraints\"" --training-portion 0.9 --num-trials 10
+#bin/mallet train-classifier --training-file unlabeled_data.mallet --testing-file unlabeled_data_test.mallet --trainer "MaxEntGETrainer,gaussianPriorVariance=0.1,constraintsFile=\"review.constraints\"" --report test:accuracy
